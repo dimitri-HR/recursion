ad5691f35651f6756da577704b21fbeab5b920c4
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-02-07 17:52:50 +1100

    Implement stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,80 +1,39 @@
 var stringifyJSON = function(obj) {
   var result = '';
 
-  if(typeof obj === 'string'){
+  if (typeof obj === 'string') {
     return result + '"' + obj + '"';
   }
-  if(typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
     return result + obj;
   }
   if (typeof obj === 'undefined') {
     return result + null;
   }
 
-
-  if(Array.isArray(obj)){
-      if(obj.length === 0) {
-        return "[]";
-      } else {
-        result =  "[";
-        for (var i = 0; i < obj.length; i++) {
-          result += stringifyJSON(obj[i]);
-          if (i < obj.length - 1) {
-            result += ",";
-          }
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return "[]";
+    } else {
+      result =  "[";
+      for (var i = 0; i < obj.length; i++) {
+        result += stringifyJSON(obj[i]);
+        if (i < obj.length - 1) {
+          result += ",";
         }
-        result += "]";
       }
+      result += "]";
     }
+  }
 
-    if (Object.prototype.toString.call(obj).slice(8, -1) === 'Object') {
-  // if(typeof obj === 'object'){
+  if (Object.prototype.toString.call(obj).slice(8, -1) === 'Object') {
     result += '{';
     for (var key in obj){
-      if(typeof obj[key] !== 'function' || typeof obj[key] !== 'undefined'){
+      if (!(typeof obj[key] === 'function' || typeof obj[key] === 'undefined')) {
         result += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
       }
     }
-    if(result.length > 1){
-      result = result.slice(0, -1) + '}';
-    } else {
-      return result + '}';
-    }
+    result = result.length > 1 ? result.slice(0, -1) + '}' : result + '}';
   }
   return result;
 };
-
-
-// var stringifyJSON = function(obj) {
-//   var result = '';
-//   if (typeof obj === 'string') {
-//     return result + '"' + obj + '"';
-//   }
-//   if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
-//     return result += obj;
-//   }
-//   if (typeof obj === 'undefined') {
-//     return result + null;
-//   }
-//
-//   if (Array.isArray(obj)) {
-//     result += '[';
-//     for (var i = 0; i < obj.length; i++) {
-//       result += stringifyJSON(obj[i]) + ',';
-//     }
-//     result = result.length > 1 ? result.slice(0, -1) + ']' : result + ']';
-//   }
-//
-//   if (Object.prototype.toString.call(obj).slice(8, -1) === 'Object') {
-//     result += '{';
-//     for (var key in obj) {
-//       if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
-//         // do nothing
-//       } else {
-//         result += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
-//       }
-//     }
-//     result = result.length > 1 ? result.slice(0, -1) + '}' : result + '}';
-//   }
-//   return result;
-// };
